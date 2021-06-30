7104ad583d8352edb5db31d1d8651a2d6ff517d9
TheCatWest <catwestover@outlook.com>
2021-06-30 14:05:05 -0700

    finish writing controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,21 +1,95 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const { Thought, User } = require('../models');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const thoughtController = {
+     //get all available thoughts
+     getAllThoughts(req, res) {
+          Thought.find({})
+          .select('-__v')
+          .then(ThoughtsDb => res.json(ThoughtsDb))
+          .catch(err => {
+               console.log(err);
+               res.sendStatus(400);
+          });
+     },
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+     //get an individual thought by id
+     getOneThought({ params }, res) {
+          Thought.findOne({ _id: params.id })
+          .select('-__v')
+          .then(ThoughtsDb => {
+               if(!ThoughtsDb) {
+                   return res.status(404).json({ message: "No user with that ID exists. Try again." });
+               }
+               res.json(ThoughtsDb);
+          })
+          .catch(err => res.json(err));
+     },
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api-18', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+     // create (post) a new thought -> push its id to user's thoughts field
+     createThought({params, body}, res) {
+        Thought.create(body)
+        .then(({_id}) => {
+            return User.findOneAndUpdate(
+                {_id: params.userId},
+                {$push: {thoughts: _id}},
+                {new: true, runValidators: true}
+            );
+        })
+        .then(UsersDb => {
+            if (!UsersDb) {
+                return res.status(404).json({ message: "No user with that ID exists. Try again." })
+            }
+            res.json(UsersDb);
+        })
+        .catch(err => res.json(err));
+    },,
 
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
+     // update (put) a thought by id
+     updateThought({ params, body }, res) {
+          Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+          .then(ThoughtsDb => {
+               if (!ThoughtsDb) {
+                   return res.status(404).json({ message: "No thought with that ID exists. Try again." });
+               }
+               res.json(ThoughtsDb);
+          })
+          .catch(err => res.json(err));
+     },
 
-app.use(require('./routes'));
+     //delete a thought by id
+     deleteThought({ params }, res) {
+          Thought.findOneAndDelete({ _id: params.id })
+          .then(ThoughtsDb => res.json(ThoughtsDb))
+          .catch(err => res.json(err));
+     },
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+     //create (post) a new reaction
+     createReaction({ params, body }, res) {
+          Thought.findOneAndUpdate({ _id: params.thoughtId }, { $push: { reactions: body} }, { new: true, runValidators: true })
+          .then(ReactionsDb => {
+               if (!ReactionsDb) {
+                    return res.status(404).json({ message: "No user with that ID exists. Try again." });
+               }
+               res.json(ReactionsDb);
+          })
+          .catch(err => res.json(err));
+     },
+
+     //delete an individual reaction
+     deleteReaction({ params, body }, res) {
+          console.log('params', params.thoughtId);
+          console.log('body', body.reactionId);
+          Thought.findOneAndUpdate(
+               { _id: params.thoughtId },
+               { $pull: { reactions: { reactionId: body.reactionId } } },
+               { new: true }
+          )
+          .then(ThoughtsDb => res.json(ThoughtsDb))
+          .catch(err => {
+               console.log(err);
+               res.json(err);
+          });
+     }
+};
+
+module.exports = thoughtController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,6 @@
-const { urlToHttpOptions } = require('http');
+
 const { Schema, model, Types } = require('mongoose');
-dateFormat = require('../utils/dateFormat');
+const dateFormat = require('../utils/dateFormat');
 
 const ThoughtSchema = new Schema(
     {
